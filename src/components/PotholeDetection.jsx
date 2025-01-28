a45a2195364ad0a5ea96b97e6a4a19dd8a2bb86d
Dionisius951 <dionisius678@gmail.com>
2025-01-29 00:16:04 +0700

    update pages

--- src/components/PotholeDetection.jsx
+++ src/components/PotholeDetection.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.APP_URL);
 
 const PotholeDetection = () => {
   const canvasRef = useRef(null);
--- src/pages/Detection.jsx
+++ src/pages/Detection.jsx
@@ -1,28 +1,85 @@
-import React from "react";
-import CameraFeed from "../components/CameraFeed";
-import PotholeDetection from "../components/PotholeDetection";
+import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://13.211.59.64:5000");
+
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
 
 const Detection = () => {
-  const handleCapture = (frame) => {
-    socket.emit("process_frame", frame);
-  };
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [detections, setDetections] = useState([]);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      })
+      .catch((err) => console.error("Error accessing webcam: ", err));
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+
+      if (video && context) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const frame = canvas.toDataURL("image/jpeg");
+        socket.emit("process_frame", frame);
+      }
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    socket.on("bbox", (data) => {
+      setDetections(data);
+    });
+
+    return () => socket.off("bbox");
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const labels = [
+      "Lubang",
+      "Melintang",
+      "Memanjang",
+      "Pinggir",
+      "Retak Buaya",
+      "Sambungan",
+    ];
+
+    if (detections.length > 0) {
+      detections.forEach((box) => {
+        const label = labels[box.class] || "Unknown";
+        context.strokeStyle = "red";
+        context.lineWidth = 2;
+        context.strokeRect(box.x, box.y, box.width, box.height);
+        context.font = "20px Arial";
+        context.fillStyle = "red";
+        context.fillText(label, box.x, box.y - 5);
+      });
+    }
+  }, [detections]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-4">Deteksi Kerusakan Jalan</h1>
-
-      {/* Kamera */}
-      <div className="relative w-[960px] h-auto">
-        <CameraFeed onCapture={handleCapture} />
-      </div>
-
-      {/* Tambah jarak antara kamera dan canvas */}
-      <div className="mt-4 w-[400px]">
-        <PotholeDetection />
-      </div>
+    <div>
+      <h1>Deteksi Kerusakan Jalan</h1>
+      <video ref={videoRef} style={{ display: "none" }} />
+      <canvas ref={canvasRef} style={{ border: "1px solid black" }} />
     </div>
   );
 };
